7742e4c94ceca8d2694f4e8dcf86511b8359d500
xiaoyu <xiaoyu@DESKTOP-A2NK211>
2017-03-12 11:40:46 +0800

    提交

--- src/com/sms/ali/AliSmsTest.java
+++ src/com/sms/ali/AliSmsTest.java
@@ -125,25 +125,4 @@ public class AliSmsTest {
         return prestr;
     }
 	
-	 private static String createLinkStringToPay(Map<String, String> params) {
-
-	        List<String> keys = new ArrayList<String>(params.keySet());
-	        Collections.sort(keys);
-
-	        String prestr = "";
-
-	        for (int i = 0; i < keys.size(); i++) {
-	            String key = keys.get(i);
-	            String value = params.get(key);
-
-	            if (i == keys.size() - 1) {//拼接时，不包括最后一个&字符
-	                prestr = prestr + key + "=" + "\""+value+"\"";
-	            } else {
-	                prestr = prestr + key + "="  + "\""+value+"\"" + "&";
-	            }
-	        }
-
-	        return prestr;
-	    }
-	
 }
