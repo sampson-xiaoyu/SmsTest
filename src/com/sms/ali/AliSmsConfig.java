402e7cddd44f943830fc3beea0a0ae70f9b84fa8
xiaoyu <xiaoyu@DESKTOP-A2NK211>
2017-03-12 11:41:33 +0800

    提交

--- src/com/sms/ali/AliSmsConfig.java
+++ src/com/sms/ali/AliSmsConfig.java
@@ -10,9 +10,9 @@ public class AliSmsConfig {
 	
 	public final static String signName = "阿里云短信服务测试";
 	
-	public final static String accessKeyId = "2088411690943421";
+	public final static String accessKeyId = "";
 	
-	public final static String accessKeySecret = "MIICdwIBADANBgkqhkiG9w0BAQEFAASCAmEwggJdAgEAAoGBAJ4vlpISpbOCRbg6CtBCcC8O6gRmKtuJPtWwrtOBi752EXZ6FrqKSzaODVqiZs8VsZpCcoZpHsM6TGrkCppow4BfCQniMExOWhbcpzKUXzc8ar3oTi/K+wTdAMwYRbv7Tq9lKFoUdCIVGgFqXciptvUYN8GcH7cjy5kDCa776VCHAgMBAAECgYBKHfwL10bidBCcwD1e1TJhKjjF9OTx1j2lvFI98b37cLC/9XZ+0tto5WgmZl+LehusLqQgjFKIoOE7WDwLmzi2SbwKeUJQ98aNOW0R9YEb3VH1BO5lmZbotvehl+NJDgeMPzONojIDghSmes9+eAIbg66vSiQzjeg8et70ZeOU+QJBAMqWUS6xjA4WW2QO8YBYARc6VUbE+YIpGx3JoSrxk6001laah3pmn+sKu8C5MXf5hIOrwEHu0SEJbCbX+sylrbsCQQDH5GSSRkRQgkAcWEyHPjcM58IreWaja7V+FcYdCIR/80vB6jaQFbbixRPlZtqTs5938QPSS8u1AruPsQWroBWlAkEAnpWIu0qU3jo2FF92QQei4vHw61MqFe/joC/ED//HoGCqIGUbBH/tU2q1iqntxYL3brM17ubpojjkBowvcZ+IHQJBAKzsaRCdlwWq2DltLa8l5EvWxoQDfwZK7HRJ89wz/SQPXm8/5bnZ/x/IbADdbKOwFXmWV776cRyDY6zX8Fbboz0CQB/3IWwzRcFhz1tJTz6OO0W3z0aNR7W58LCEu3ukOpp8mCezULWa/JeqLq7nmJVwHlmWwZ8TdCdgdcsaLYlMrAU=";
+	public final static String accessKeySecret = "";
 	
 	public final static String signatureVersion = "1.0";
 	
